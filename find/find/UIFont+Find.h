86e9ce982082dd6be92569a0c46414bee69bc598
Bin <30135878@qq.com>
2013-10-14 10:05:04 +0800

    Add theme font

---
+++ find/find/UIFont+Find.h
@@ -0,0 +1,15 @@
+//
+//  UIFont+Find.h
+//  find
+//
+//  Created by zhangbin on 10/14/13.
+//  Copyright (c) 2013 ZoomBin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIFont (Find)
+
++ (UIFont *)fdThemeFontWithSize:(CGFloat)size;
+
+@end
---
+++ find/find/UIFont+Find.m
@@ -0,0 +1,18 @@
+//
+//  UIFont+Find.m
+//  find
+//
+//  Created by zhangbin on 10/14/13.
+//  Copyright (c) 2013 ZoomBin. All rights reserved.
+//
+
+#import "UIFont+Find.h"
+
+@implementation UIFont (Find)
+
++ (UIFont *)fdThemeFontWithSize:(CGFloat)size
+{
+	return [UIFont fontWithName:@"FZBWKSJW--GB1-0" size:size];
+}
+
+@end
