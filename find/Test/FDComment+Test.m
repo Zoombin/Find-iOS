ec9058c49db56a43eb129eb3b7903a5cc413227f
Bin <30135878@qq.com>
2013-11-02 14:48:09 +0800

    Remove useless codes

--- find/Test/FDComment+Test.m
+++ find/Test/FDComment+Test.m
@@ -8,24 +8,6 @@
 
 #import "FDComment+Test.h"
 
-//@property (nonatomic, strong) NSNumber *ID;
-//@property (nonatomic, strong) NSNumber *userID;
-//@property (nonatomic, strong) NSString *username;
-//@property (nonatomic, strong) NSString *content;
-//@property (nonatomic, strong) NSNumber *typeID;//评论对象id
-//@property (nonatomic, strong) NSNumber *type;
-//@property (nonatomic, strong) NSNumber *published;
-
-//"id": 17,
-//"mid": 1,
-//"username": "root",
-//"content": "comment at:2013-23-22 17:10:09",
-//"typeid": 1,
-//"type": 0,
-//"publiship": 1884701257,
-//"published": 1382433823
-
-
 @implementation FDComment (Test)
 
 + (NSArray *)createTest:(NSUInteger)count
@@ -38,12 +20,5 @@
 	}
 	return comments;
 }
-
-//+ (FDComment *)createTestOne
-//{
-//	FDComment *user = [[FDUser alloc] init];
-//	user.photos = [FDPhoto createTest:100];
-//	return user;
-//}
 
 @end
