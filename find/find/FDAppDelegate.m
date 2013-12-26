2221fa49e94f2a14f187dec57441116cd643edbc
Bin <30135878@qq.com>
2013-12-26 10:16:38 +0800

    Add Umeng

--- find/find/FDAppDelegate.m
+++ find/find/FDAppDelegate.m
@@ -54,6 +54,7 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
 	[self test];
+	[self umengTrack];
 	if (SYSTEM_VERSION_GREATER_THAN_OR_EQUAL_TO(@"7.0")) {
 		[application setStatusBarStyle:UIStatusBarStyleLightContent];
 	} else {
@@ -125,6 +126,30 @@
 - (void)applicationWillTerminate:(UIApplication *)application
 {
 	// Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+}
+
+#pragma mark - UMeng
+
+- (void)umengTrack
+{
+	[MobClick setCrashReportEnabled:YES]; // 如果不需要捕捉异常，注释掉此行
+    [MobClick setLogEnabled:YES];  // 打开友盟sdk调试，注意Release发布时需要注释掉此行,减少io消耗
+    //[MobClick setAppVersion:XcodeAppVersion]; //参数为NSString * 类型,自定义app版本信息，如果不设置，默认从CFBundleVersion里取
+    [MobClick startWithAppkey:UMENG_APPKEY reportPolicy:(ReportPolicy) REALTIME channelId:nil];
+	//reportPolicy为枚举类型,可以为 REALTIME, BATCH,SENDDAILY,SENDWIFIONLY几种
+	//channelId 为NSString * 类型，channelId 为nil或@""时,默认会被被当作@"App Store"渠道
+	
+	//[MobClick checkUpdate];   //自动更新检查, 如果需要自定义更新请使用下面的方法,需要接收一个(NSDictionary *)appInfo的参数
+	//[MobClick checkUpdateWithDelegate:self selector:@selector(updateMethod:)];
+	
+    [MobClick updateOnlineConfig];  //在线参数配置
+	
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(onlineConfigCallBack:) name:UMOnlineConfigDidFinishedNotification object:nil];
+}
+
+- (void)onlineConfigCallBack:(NSNotification *)note {
+    
+    NSLog(@"online config has fininshed and note = %@", note.userInfo);
 }
 
 @end
