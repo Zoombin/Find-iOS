a45ad2baa35457dbfdce0adb6f3c2ab3b82aeae1
Bin <30135878@qq.com>
2013-12-30 12:44:22 +0800

    Update error message

--- find/find/FDErrorMessage.h
+++ find/find/FDErrorMessage.h
@@ -8,6 +8,11 @@
 
 #import <Foundation/Foundation.h>
 
+extern NSInteger PHOTO_LIKE;
+extern NSInteger FORBIDDEN;
+extern NSInteger NEW_PASSWORD_EMPTY;
+extern NSInteger AGAIN_PASSWORD_EMPTY;
+extern NSInteger AGAIN_PASSWORD_WRONG;
 extern NSInteger SYSTEM_ERROR;
 extern NSInteger USERNAME_EMPTY;
 extern NSInteger PASSWORD_EMPTY;
@@ -18,11 +23,17 @@ extern NSInteger USERNAME_ERROR;
 extern NSInteger CANNOT_FOLLOW_SELF;
 extern NSInteger PHOTO_DONOT_EXISTS;
 extern NSInteger CONTENT_EMPTY;
-extern NSInteger TAG_DONOT_EXISTS;
+extern NSInteger CONTENT_FORBIDDEN;
 extern NSInteger DOLIKE;
 extern NSInteger DOUNLIKE;
+extern NSInteger DOADD;
+extern NSInteger DOREMOVE;
 extern NSInteger DOFOLLOW;
 extern NSInteger DOUNFOLLOW;
+extern NSInteger TAG_DONOT_EXISTS;
+extern NSInteger CATEGORY_DONOT_EXISTS;
+extern NSInteger SHOP_SOLD_OUT;
+extern NSInteger MONEY_NOT_ENOUGH;
 
 @interface FDErrorMessage : NSObject
 
--- find/find/FDErrorMessage.m
+++ find/find/FDErrorMessage.m
@@ -8,6 +8,11 @@
 
 #import "FDErrorMessage.h"
 
+NSInteger PHOTO_LIKE = 3;
+NSInteger FORBIDDEN = 403;
+NSInteger NEW_PASSWORD_EMPTY = 20021;
+NSInteger AGAIN_PASSWORD_EMPTY = 20022;
+NSInteger AGAIN_PASSWORD_WRONG = 20023;
 NSInteger SYSTEM_ERROR = 1001;
 NSInteger USERNAME_EMPTY = 2001;
 NSInteger PASSWORD_EMPTY = 2002;
@@ -18,11 +23,17 @@ NSInteger USERNAME_ERROR = 2006;
 NSInteger CANNOT_FOLLOW_SELF = 2007;
 NSInteger PHOTO_DONOT_EXISTS = 3001;
 NSInteger CONTENT_EMPTY = 3002;
-NSInteger TAG_DONOT_EXISTS = 4001;
+NSInteger CONTENT_FORBIDDEN = 3003;
 NSInteger DOLIKE = 10;
 NSInteger DOUNLIKE = 11;
+NSInteger DOADD = 12;
+NSInteger DOREMOVE = 13;
 NSInteger DOFOLLOW = 14;
 NSInteger DOUNFOLLOW = 15;
+NSInteger TAG_DONOT_EXISTS = 4001;
+NSInteger CATEGORY_DONOT_EXISTS = 4002;
+NSInteger SHOP_SOLD_OUT = 5001;
+NSInteger MONEY_NOT_ENOUGH = 5002;
 
 @implementation FDErrorMessage
 
@@ -31,20 +42,31 @@ static NSDictionary *errorMessageMap;
 + (NSString *)messageFromData:(id)msgData
 {
 	if (!errorMessageMap) {
-		errorMessageMap = @{@(SYSTEM_ERROR) : @"服务异常，请检测您的网络！",
-							@(USERNAME_EMPTY) : @"用户名不能为空！",
-							@(PASSWORD_EMPTY) : @"密码不能为空！",
-							@(USER_DONOT_EXISTS) : @"用户不存在！",
-							@(PASSWORD_WRONG) : @"密码错误！",
-							@(USER_EXISTS) : @"用户已存在！",
-							@(CANNOT_FOLLOW_SELF) : @"不能关注自己！",
-							@(PHOTO_DONOT_EXISTS) : @"照片不存在！",
-							@(CONTENT_EMPTY) : @"内容为空！",
-							@(DOLIKE) : @"喜欢",
-							@(DOUNLIKE) : @"不喜欢",
-							@(TAG_DONOT_EXISTS) : @"标签不存在！",
-							@(DOFOLLOW) : NSLocalizedString(@"已关注", @"关注后返回的结果"),
-							@(DOUNFOLLOW) : NSLocalizedString(@"取消关注", @"取消关注后返回的结果")
+		errorMessageMap = @{
+							@(PHOTO_LIKE) : NSLocalizedString(@"喜欢照片", nil),
+							@(FORBIDDEN) : NSLocalizedString(@"操作无效", nil),
+							@(NEW_PASSWORD_EMPTY) : NSLocalizedString(@"新密码不能为空", nil),
+							@(AGAIN_PASSWORD_EMPTY) : NSLocalizedString(@"确认密码不能为空", nil),
+							@(AGAIN_PASSWORD_WRONG) : NSLocalizedString(@"两次密码输入不一致", nil),
+							@(SYSTEM_ERROR) : NSLocalizedString(@"服务异常，请检测您的网络！", nil),
+							@(USERNAME_EMPTY) : NSLocalizedString(@"用户名不能为空！", nil),
+							@(PASSWORD_EMPTY) : NSLocalizedString(@"密码不能为空！", nil),
+							@(USER_DONOT_EXISTS) : NSLocalizedString(@"用户不存在！", nil),
+							@(PASSWORD_WRONG) : NSLocalizedString(@"密码错误！", nil),
+							@(USER_EXISTS) : NSLocalizedString(@"用户已存在！", nil),
+							@(USERNAME_ERROR) : NSLocalizedString(@"用户名错误", nil),
+							@(CANNOT_FOLLOW_SELF) : NSLocalizedString(@"不能关注自己！", nil),
+							@(PHOTO_DONOT_EXISTS) : NSLocalizedString(@"照片不存在！", nil),
+							@(CONTENT_EMPTY) : NSLocalizedString(@"内容为空！", nil),
+							@(CONTENT_FORBIDDEN) : NSLocalizedString(@"内容被禁止", nil),
+							@(DOLIKE) : NSLocalizedString(@"喜欢", nil),
+							@(DOUNLIKE) : NSLocalizedString(@"不喜欢", nil),
+							@(DOADD) : NSLocalizedString(@"添加", nil),
+							@(DOREMOVE) : NSLocalizedString(@"删除", nil),
+							@(TAG_DONOT_EXISTS) : NSLocalizedString(@"标签不存在！", nil),
+							@(CATEGORY_DONOT_EXISTS) : NSLocalizedString(@"部位不存在", nil),
+							@(SHOP_SOLD_OUT) : NSLocalizedString(@"已售完", nil),
+							@(MONEY_NOT_ENOUGH) : NSLocalizedString(@"余额不足", nil)
  							};
 	}
 	
